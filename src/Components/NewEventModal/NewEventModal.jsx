6fe7628efef5b0b8a85a6c902877a95ee007a94a
Josh Hankins <jhankins0903@gmail.com>
2023-05-21 21:56:55 -0400

    updated neweventmodal

--- src/Components/NewEventModal/NewEventModal.jsx
+++ src/Components/NewEventModal/NewEventModal.jsx
@@ -7,10 +7,9 @@ import { useSelector } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
 import ModalWithOverlay from "../ReusableComponents/ModalWithOverlay/ModalWithOverlay";
 
-import { db } from "E:/Web Development/React Projects/calendar/src/firebase/config.js";
 // import { doc, updateDoc } from "firebase/firestore";
 
-// import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 
 import { AuthContext } from "E:/Web Development/React Projects/calendar/src/Contexts/AuthContext.jsx";
 
@@ -62,12 +61,12 @@ const NewEventModal = ({ setIsOpen }) => {
 
     console.log(allEvents);
 
-    // const docRef = doc(db, "users", currentUser.uid);
+    const docRef = doc(db, "users", currentUser.uid);
 
-    // updateDoc(docRef, {
-    //   email: currentUser.email,
-    //   events: [...allEvents, newEventObj],
-    // });
+    updateDoc(docRef, {
+      email: currentUser.email,
+      events: [...allEvents, newEventObj],
+    });
 
     setIsOpen(false);
   }
