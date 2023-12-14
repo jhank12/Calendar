da139207a1cdc2fa924f976a87bc8d429ffc756d
Josh Hankins <jhankins0903@gmail.com>
2023-12-13 20:12:14 -0500

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ function App() {
     <AuthContextProvider>
       <main>
         <Routes>
-          <Route path="/*" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/*" exact element={<Login />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/forgot-password" exact element={<ForgotPassword />} />
 
           <Route element={<ProtectedRoutes />}>
-            <Route path="/home" element={<Home />} exact />
+            <Route path="/home" exact element={<Home />} />
           </Route>
         </Routes>
       </main>
--- src/Components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -12,13 +12,9 @@ import { auth } from "../../firebase/config";
 
 const ProtectedRoutes = () => {
   const { currentUser } = useContext(AuthContext);
-  console.log(currentUser)
   const user = currentUser || JSON.parse(localStorage.getItem("user"));
-  console.log(JSON.parse(localStorage.getItem('user')))
-  
+
   return <>{user ? <Outlet /> : <Login />}</>;
-  return <h1>{JSON.stringify(user)}</h1> 
-  // return <>{currentUser ? <Outlet /> : <Login />}</>;
 };
 
 export default ProtectedRoutes;
