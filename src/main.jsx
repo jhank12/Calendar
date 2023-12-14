f706614cf99468cb4175a180220a797b50e76776
Josh Hankins <jhankins0903@gmail.com>
2023-12-13 21:05:22 -0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 
 import { store } from "./Redux/store";
 
@@ -13,9 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
 
   <Provider store={store}>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </Provider>
 
   // </React.StrictMode>,
